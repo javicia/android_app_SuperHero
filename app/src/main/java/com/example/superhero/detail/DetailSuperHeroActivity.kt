66e8e413ec31cc9880b5fe9d28a714d9dd10f6df
Javier García <javigpdeveloper@gmail.com>
2023-12-18 14:32:41 +0100

    Pintar todas las vistas

--- app/src/main/java/com/example/superhero/detail/DetailSuperHeroActivity.kt
+++ app/src/main/java/com/example/superhero/detail/DetailSuperHeroActivity.kt
@@ -1,51 +1,75 @@
 package com.example.superhero.detail
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import androidx.core.view.isVisible
-import com.example.superhero.R
+import android.util.TypedValue
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 import com.example.superhero.api.ApiService
-import com.example.superhero.response.SuperHeroDataResponse
+import com.example.superhero.databinding.ActivityDetailSuperHeroBinding
+import com.example.superhero.response.PowerStatsResponse
+import com.example.superhero.response.SuperHeroDetailResponse
+import com.squareup.picasso.Picasso
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
-import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import kotlin.math.roundToInt
 
 class DetailSuperHeroActivity : AppCompatActivity() {
 
-    companion object{
-        const val EXTRA_ID ="extra_id"
+    companion object {
+        const val EXTRA_ID = "extra_id"
     }
+
+    private lateinit var binding: ActivityDetailSuperHeroBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_detail_super_hero)
-        val id:String = intent.getStringExtra(EXTRA_ID).orEmpty()
+        binding = ActivityDetailSuperHeroBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val id: String = intent.getStringExtra(EXTRA_ID).orEmpty()
         getSuperheroInformation(id)
     }
 
     private fun getSuperheroInformation(id: String) {
         //Hilo secundario IO
         CoroutineScope(Dispatchers.IO).launch {
-            getRetrofit()create(ApiService::class.java).getSuperHero(query)
-            val myResponse: Response<SuperHeroDataResponse> =
-                retrofit.create(ApiService::class.java).getSuperHero(query)
-            if (myResponse.isSuccessful) {
-                Log.i("Javicia", "funciona: ")
-                var response: SuperHeroDataResponse? = myResponse.body()
-                if(response != null){
-                    Log.i("javicia", response.toString())
-                    runOnUiThread{
-                        adapter.updateList(response.superheroes)
-                        binding.progressBar.isVisible = false
-                    }
-                }
-            }else{
-                Log.i("javicia", "no funciona")
+            var superHeroDetails =
+                getRetrofit().create(ApiService::class.java).getSuperHeroDetail(id)
+            if (superHeroDetails.body() != null) {
+                runOnUiThread { createUI(superHeroDetails.body()!!) }
+
             }
+
         }
+    }
+
+    private fun createUI(superhero: SuperHeroDetailResponse) {
+        Picasso.get().load(superhero.image.url).into(binding.ivSuperHero)
+        binding.tvSuperheroName.text = superhero.name
+        prepareStats(superhero.powerstats)
+        binding.tvSuperheroRealName.text=superhero.biography.fullName
+        binding.tvPublisher.text=superhero.biography.publisher
+    }
+
+    private fun prepareStats(powerstats: PowerStatsResponse) {
+        updateHeight(binding.viewCombat, powerstats.combat)
+        updateHeight(binding.viewIntelligence, powerstats.intelligence)
+        updateHeight(binding.viewStrength, powerstats.strength)
+        updateHeight(binding.viewSpeed, powerstats.speed)
+        updateHeight(binding.viewDurability, powerstats.durability)
+        updateHeight(binding.viewPower, powerstats.power)
+    }
+
+    private fun updateHeight(view: View, stat: String) {
+        val parans = view.layoutParams
+        parans.height = pxUp(stat.toFloat())
+        view.layoutParams = parans
+    }
+
+    private fun pxUp(px: Float): Int {
+        //transformar dp a pixeles
+        return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, px, resources.displayMetrics).roundToInt()
     }
 
     private fun getRetrofit(): Retrofit {
--- app/src/main/java/com/example/superhero/response/SuperHeroDetailResponse.kt
+++ app/src/main/java/com/example/superhero/response/SuperHeroDetailResponse.kt
@@ -3,7 +3,9 @@ package com.example.superhero.response
 import com.google.gson.annotations.SerializedName
 
 data class SuperHeroDetailResponse(@SerializedName("name") val name: String,
-                                   @SerializedName("powerstats") val powerstats: String)
+                                   @SerializedName("powerstats") val powerstats: PowerStatsResponse,
+                                   @SerializedName("image") val image: SuperheroImageDetailsResponse,
+                                   @SerializedName("biography") val biography: Biography)
 data class PowerStatsResponse(
     @SerializedName("intelligence") val intelligence: String,
     @SerializedName("strength") val strength: String,
@@ -11,5 +13,13 @@ data class PowerStatsResponse(
     @SerializedName("durability") val durability: String,
     @SerializedName("power") val power: String,
     @SerializedName("combat") val combat: String,
-
     )
+
+data class SuperheroImageDetailsResponse(
+    @SerializedName("url") val url: String
+)
+
+data class Biography(
+    @SerializedName("full-name") val fullName:String,
+    @SerializedName("publisher") val publisher:String
+)
